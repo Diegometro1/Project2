65c93338b104c92216e234833dbe80fea84f74f7
Diego Acosta <diegometro1@hotmail.com>
2017-09-19 01:01:32 -0400

    updating css img index

--- index.php
+++ index.php
@@ -6,39 +6,39 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Password Generator</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/project.css">
 </head>
 <body>
 
-<h1>Probando conection</h1>
 
-<p>Diego's Password Generator Project 2</p>
+<h1>Password Generator: Project 2</h1>
 
-<img src="images/images.jpeg" alt="Mountain View" style="width:304px;height:228px;">
+<img src="img/images.jpeg" alt="Mountain View" style="width:304px;height:228px;">
 
 <form action=" " method="post">
 
-    Name: <input type="text" name="name" required><span class="error"></span>
+    Name: <input type="text" name="name" required><span class="error"><?php echo $nameErr;?></span>
     <br><br>
 
-    Lastname: <input type="text" name="lastname" required><span class="error"></span>
+    Lastname: <input type="text" name="lastname" required><span class="error"><?php echo $lastnameErr;?></span>
     <br><br>
 
-    E-mail: <input type="text" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"><span class="error"></span>
+    E-mail: <input type="text" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"><span class="error"><?php echo $emailErr;?></span>
     <br><br>
 
     Gender:
     <input type="radio" name="gender" value="female">Female
     <input type="radio" name="gender" value="male">Male
-    <span class="error"></span>
+    <span class="error"><?php echo $genderErr;?></span>
     <br><br>
 
-    Length: <input type="text" name="length" value="" /><br />
+    Length: <input type="text" name="length" value="<?php if(isset($_POST['length'])) { echo $_POST['length']; } ?>" /><br />
     <br />
-    <input type="checkbox" name="lower" value="1" /> Lowercase<br />
-    <input type="checkbox" name="upper" value="1"  /> Uppercase<br />
-    <input type="checkbox" name="numbers" value="1" /> Numbers<br />
-    <input type="checkbox" name="symbols" value="1" /> Symbols<br />
+    <input type="checkbox" name="lower" value="1" <?php if($_POST['lower'] == 1) { echo 'checked'; } ?> /> Lowercase<br />
+    <input type="checkbox" name="upper" value="1" <?php if($_POST['upper'] == 1) { echo 'checked'; } ?> /> Uppercase<br />
+    <input type="checkbox" name="numbers" value="1" <?php if($_POST['numbers'] == 1) { echo 'checked'; } ?> /> Numbers<br />
+    <input type="checkbox" name="symbols" value="1" <?php if($_POST['symbols'] == 1) { echo 'checked'; } ?> /> Symbols<br />
 
     <input type="submit" value="Submit" id= "submit"/>
 
@@ -46,7 +46,8 @@
 
 
 
-<p id = "password">Generated Password: </p>
+<p id = "password">Generated Password: <?php echo $password; ?></p>
+
 
 </body>
 
